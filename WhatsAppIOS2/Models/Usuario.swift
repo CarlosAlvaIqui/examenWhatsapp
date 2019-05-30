6f42d07da7fc26dcb3f7fa1e918920b6989756cb
tecsup <example@gmail.com>
2019-05-29 19:42:17 -0500

    avanze en casa plox

---
+++ WhatsAppIOS2/Models/Usuario.swift
@@ -0,0 +1,15 @@
+//
+//  Usuario.swift
+//  WhatsAppIOS2
+//
+//  Created by MAC11 on 29/05/19.
+//  Copyright © 2019 Carlos Alvarez. All rights reserved.
+//
+
+import Foundation
+
+class Usuario{
+    var email = ""
+    var photoURL = ""
+    var uid = ""
+}
--- WhatsAppIOS2/ViewControllers/ListaUsuariosViewController.swift
+++ WhatsAppIOS2/ViewControllers/ListaUsuariosViewController.swift
@@ -11,25 +11,39 @@ import Firebase
 import FirebaseAuth
 
 class ListaUsuariosViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
 
-    
+    @IBOutlet weak var listaUsuarios: UITableView!
+    var usuarios:[Usuario] = []
     
     @IBAction func cerrarSesionTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usuarios.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let usuario = usuarios[indexPath.row]
+        cell.textLabel?.text = usuario.email
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        listaUsuarios.delegate = self
+        listaUsuarios.dataSource = self
+        
+        Database.database().reference().child("usuarios").observe(DataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+        })
+        /*
+        let usuario = Usuario()
+    usuario.email = (snapshot.value as! NSDictionary)["email"] as! String
+        usuario.uid = snapshot.key
+        self.usuarios.append(usuario)
+        self.listaUsuarios.roloadData()
+        // Do any additional setup after loading the view.*/
     }
     
 
